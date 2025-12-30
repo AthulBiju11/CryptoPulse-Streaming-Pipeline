f2694f76ca74ddd11cde25821b0cd51d58f1192d
AthulBiju11 <AthulBiju11>
2025-12-30 20:46:53 +0000

    Dashboard dataset queries and preparation complete

--- etl_notebooks/silver/fact_crypto_trades_load.py
+++ etl_notebooks/silver/fact_crypto_trades_load.py
@@ -2,6 +2,8 @@
 from pyspark.sql.functions import from_json,col,current_timestamp
 from pyspark.sql.types import StructType,StructField,StringType
 
+checkpoint_path = "/Volumes/cryptopulse_catalog/silver/checkpoints/fact_crypto_trades"
+
 # COMMAND ----------
 
 json_schema = StructType([
@@ -14,9 +16,14 @@ json_schema = StructType([
     StructField("type", StringType(), True),
 ])
 
-bronze_df = (spark.readStream.table("cryptopulse_catalog.bronze.coinbase_raw_trades"))
+bronze_df = (spark.readStream
+    .format("delta")
+    .option("ignoreChanges", "true")
+    .table("cryptopulse_catalog.bronze.coinbase_raw_trades")
+)
 
-silver_df = bronze_df.withColumn("json_data",from_json(col("body"),json_schema)) \
+
+silver_df_uncleaned = bronze_df.withColumn("json_data", from_json(col("body"), json_schema)) \
     .select(
         col("json_data.trade_id"),
         col("json_data.product_id").alias("symbol"),
@@ -24,20 +31,18 @@ silver_df = bronze_df.withColumn("json_data",from_json(col("body"),json_schema))
         col("json_data.last_size").cast("decimal(18,8)").alias("trade_size"),
         col("json_data.side"),
         col("json_data.time").cast("timestamp").alias("event_time"),
-        col("enqueuedTime").alias("ingestion_time"),
+        col("enqueuedTime").alias("ingestion_time"),  # Pass this through for latency calcs
         current_timestamp().alias("load_timestamp")
     ) \
     .filter(col("symbol").isNotNull())
 
-display(silver_df)
-
-checkpoint_path = "/Volumes/cryptopulse_catalog/silver/checkpoints/fact_crypto_trades"
-
+silver_df = silver_df_uncleaned \
+    .withWatermark("event_time", "1 minute") \
+    .dropDuplicates(["trade_id"])
 (
     silver_df.writeStream
     .format("delta")
-    .outputMode("append")
-    .option("checkpointLocation",checkpoint_path)
-    .trigger(processingTime='10 seconds')
+    .outputMode("append")  # You can now use the faster 'append' mode
+    .option("checkpointLocation", checkpoint_path)
     .toTable("cryptopulse_catalog.silver.fact_crypto_trades")
 )
